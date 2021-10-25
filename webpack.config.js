24238fcdfd79501d60635ac936e5f709aa7ea950
sleepy dawn <devblue0813@gmail.com>
2021-10-25 23:30:59 +0530

    add console log

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const webpackDevConfig = require("./webpack.dev");
 const webpackProdConfig = require("./webpack.prod");
 
 module.exports = (env) => {
+  // console.log("checking env: ", env);
   if (env.production) {
     return webpackProdConfig;
   }
