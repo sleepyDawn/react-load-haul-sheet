4d0d430e2ab1609223bf44cad868a6b146405a51
sleepy dawn <devblue0813@gmail.com>
2021-10-27 13:38:49 +0530

    add http method to keep horoku server alive

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
 const app = express();
 
@@ -9,6 +10,14 @@ app.use(express.static(publicPath));
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
+
+setInterval(() => {
+  http.get("https://firebase-load-haul-sheet.herokuapp.com/")
+}, 300000)
+
+
+
+
 
 app.listen(port, () => {
   console.log("server is up at port- ", port);
